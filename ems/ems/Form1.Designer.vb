0bac15884720d580a8ccca134ab03cb0982991ea
jesseM15 <muddybuzzy@gmail.com>
2015-04-01 19:47:01 -0400

    database setup test

---
+++ ems/ems/Form1.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dgvUsers = New System.Windows.Forms.DataGridView()
+        CType(Me.dgvUsers, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dgvUsers
+        '
+        Me.dgvUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvUsers.Location = New System.Drawing.Point(12, 12)
+        Me.dgvUsers.Name = "dgvUsers"
+        Me.dgvUsers.Size = New System.Drawing.Size(802, 319)
+        Me.dgvUsers.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(826, 428)
+        Me.Controls.Add(Me.dgvUsers)
+        Me.Name = "Form1"
+        Me.Text = "Employee Management System"
+        CType(Me.dgvUsers, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents dgvUsers As System.Windows.Forms.DataGridView
+
+End Class
---
+++ ems/ems/Form1.vb
@@ -0,0 +1,9 @@
+﻿Imports DBlib.DBSQL
+
+Public Class Form1
+    Private dbems As New dbconnection(My.Settings.DbEmsConnectionString)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        dgvUsers.DataSource = dbems.getDataTable("Users")
+    End Sub
+End Class
